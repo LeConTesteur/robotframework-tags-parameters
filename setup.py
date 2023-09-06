f269d8f60bd6eccdfa62cddfc0a8766323df7335
richard.turc <richard.turc@stormshield.eu>
2023-09-06 15:51:38 +0200

    Setup of project

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = []
+
+requirements_tests = [
+    "requests_mock",
+    "flake8",
+    "coverage"
+]
+
+extras = {
+    'tests': requirements_tests,
+}
+
+setuptools.setup(
+    name="robotframework-tags-parameters",
+    version="0.0.1",
+    author="LeConTesteur",
+    description="Can use and parse parameters from robotframework test tags",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeConTesteur/robotframework-tags-parameters",
+    project_urls={
+        "Bug Tracker": "https://github.com/LeConTesteur/robotframework-tags-parameters/issues",
+    },
+    classifiers=[ "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires = requirements,
+    tests_require = requirements_tests,
+    extras_require = extras,
+
+)
